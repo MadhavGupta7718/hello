b6f31df51c716f1335f719228fc1a561ec750086
MadhavGupta7718 <guptamadhav7718@gmail.com>
2024-10-20 16:51:07 +0530

    Add files via upload

---
+++ bmi calculator.py
@@ -0,0 +1,27 @@
+# print("welcome to BMI calculator")
+# height = float(input("what is your height in meter?"))
+# weight = int(input("what is your approx weight in kg?"))
+# BMI = (weight/height**2)
+# print("your BMI is " + str(BMI))
+# print("If your BMI is less than 18.5, it falls within the underweight range. If your BMI is 18.5 to 24.9, it falls within the Healthy Weight range. If your BMI is 25.0 to 29.9, it falls within the overweight range. If your BMI is 30.0 or higher, it falls within the obese range")
+
+# for i range(4):
+#     for j in range(3):
+#         print(i,j)
+
+# for i in range(3):
+
+#     for j in range(2):
+#         print(i,j)
+
+# number = [1,2,3,4]
+# if len(number)>2:
+#     for num in number:
+#         print(num)
+
+# list = ["apple, banana, cherry"]
+# APPEND: add element at the last of list
+# list.append("kivi")
+
+squares = [x**2 for x in range(1000000000000)]
+print(squares)
